a7ba6b02a0105b1306dd1b65635115de54483fed
Danylo Bondarenko <bondanil01@gmail.com>
2024-08-31 16:32:08 +0300

    Add avatars to views (finish #9)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,6 +74,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
+  config.action_controller.default_url_options = { host: "localhost", port: 3000 }
+  # config.action_controller.default_url_options = { host: "2ef6-188-163-60-115.ngrok-free.app" }
 
   config.assets.debug = true
 
